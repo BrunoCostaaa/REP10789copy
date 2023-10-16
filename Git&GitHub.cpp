aa2d28bcde207f89644ec5a273e7f14e72ab38a8
Bruno Costa <brunocosta_94@hotmail.com>
2023-10-16 15:36:06 +0100

    first commit copy

---
+++ Git&GitHub.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float nota1, nota2, nota3, nota4;
+    
+    cout << "Digite a primeira nota: ";
+    cin >> nota1;
+
+    cout << "Digite a segunda nota: ";
+    cin >> nota2;
+
+    cout << "Digite a terceira nota: ";
+    cin >> nota3;
+
+    cout << "Digite a quarta nota: ";
+    cin >> nota4;
+
+    float media = (nota1 * 0.5) + (nota2 * 0.1) + (nota3 * 0.1) + (nota4 * 0.3);
+
+    cout << "Notas inseridas: " << nota1 << ", " << nota2 << ", " << nota3 << ", " << nota4 << endl;
+    cout << "Media ponderada das notas: " << media << endl;
+
+    return 0;
+}
+
